f998cc12b421e53199e20cc7bad30cb349c00191
jae <wogud1514@naver.com>
2022-01-30 16:32:51 +0900

    환경세팅

---
+++ src/test/java/springaop/jjfactory/JjfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package springaop.jjfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjfactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
