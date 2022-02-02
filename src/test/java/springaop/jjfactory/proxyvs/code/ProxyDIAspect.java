b37d2b7ac751eb365a4f1c9f07d2e975f58ba1e2
jae <wogud1514@naver.com>
2022-02-02 23:13:19 +0900

    cglib 구체클래스di 차이까지

---
+++ src/test/java/springaop/jjfactory/proxyvs/code/ProxyDIAspect.java
@@ -0,0 +1,15 @@
+package springaop.jjfactory.proxyvs.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class ProxyDIAspect {
+    @Before("execution(* springaop.jjfactory..*.*(..))")
+    public void dpTrace(JoinPoint joinPoint){
+        log.info("[proxyDIAdvice] {}",joinPoint.getSignature());
+    }
+}
